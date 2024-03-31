09d3c42512148430c8522ac0685d3bb296eefac2
Srushti-9 <srushtisuvarna77@gmail.com>
2024-03-31 15:54:22 +0530

    Weather app functional

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import SiteNavigation from './components/SiteNavigation.vue';
+</script>
+
+<template>
+  
+    <div class="flex flex-column min-h-screen font-Roboto bg-weather-primary">
+      <SiteNavigation/>
+      <RouterView/>
+    </div>
+
+  
+</template>
+
+<style lang="scss" scoped>
+
+</style>
